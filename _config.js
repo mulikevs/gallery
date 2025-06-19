7d4e6133622a317f9296004942e527e60ac54d62
mulikevs <mulikevs@gmail.com>
2025-06-19 21:29:47 +0300

    update: Added the mongodb credentials

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mulikevs-test:oLa87sWa8UM4cckL@darkroom.35hl5fl.mongodb.net/?retryWrites=true&w=majority&appName=darkroom',
+    development: 'mongodb+srv://mulikevs-test:oLa87sWa8UM4cckL@darkroom.35hl5fl.mongodb.net/?retryWrites=true&w=majority&appName=darkroom',
+    test: 'mongodb+srv://mulikevs-test:oLa87sWa8UM4cckL@darkroom.35hl5fl.mongodb.net/?retryWrites=true&w=majority&appName=darkroom',
 }
 module.exports = config;
